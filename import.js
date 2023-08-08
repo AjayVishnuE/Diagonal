9386c8f2cd1e1d6afa35c1c065475ea421e4e753
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-08-08 20:39:52 +0530

    js commit

---
+++ import.js
@@ -0,0 +1,10 @@
+function switchVisible() {
+    document.getElementById('center-image-holder').style.display == 'none';
+    document.getElementById('center-logo-img').style.display == 'none';
+    document.getElementById('center-appear-downarrow').style.display = 'block';
+    document.getElementById('center-line-1').style.display = 'block';
+    document.getElementById('center-line-2').style.display = 'block';
+    document.getElementById('center-arrow-appear-sec1').style.display = 'flex';
+    document.getElementById('center-arrow-appear-sec2').style.display = 'flex';
+    document.getElementById('center-arrow-appear-sec3').style.display = 'flex';
+}
