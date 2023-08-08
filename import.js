b7ce79cc48d3952cedd5b838b075a47826698d3c
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-08-08 22:39:16 +0530

    main commit

--- import.js
+++ import.js
@@ -6,6 +6,11 @@ function arrowClicked() {
     document.getElementById('center-arrow-appear-sec1').style.display = 'flex';
     document.getElementById('center-arrow-appear-sec2').style.display = 'flex';
     document.getElementById('center-arrow-appear-sec3').style.display = 'flex';
+    document.getElementById('center-appear-work').style.display = 'none';
+    document.getElementById('center-appear-about').style.display = 'none';
+    document.getElementById('center-appear-tech').style.display = 'none';
+    document.getElementById('center-appear-careers').style.display = 'none';
+    document.getElementById('center-appear-contact').style.display = 'none';
 }
 
 function homeClicked(){
@@ -16,29 +21,45 @@ function homeClicked(){
 function workClicked(){
     document.getElementById('center-image-holder').style.display == 'none';
     document.getElementById('center-appear-work').style.display = 'flex';
-    // document.getElementById('center-appear-about').style.display = 'flex';
-    // document.getElementById('center-appear-tech').style.display = 'flex';
-    // document.getElementById('center-appear-careers').style.display = 'flex';
-    // document.getElementById('center-appear-contact').style.display = 'flex';
+    document.getElementById('center-appear-about').style.display = 'none';
+    document.getElementById('center-appear-tech').style.display = 'none';
+    document.getElementById('center-appear-careers').style.display = 'none';
+    document.getElementById('center-appear-contact').style.display = 'none';
 
 }
 
 function aboutClicked(){
     document.getElementById('center-image-holder').style.display == 'none';
+    document.getElementById('center-appear-work').style.display = 'none';
     document.getElementById('center-appear-about').style.display = 'flex';
+    document.getElementById('center-appear-tech').style.display = 'none';
+    document.getElementById('center-appear-careers').style.display = 'none';
+    document.getElementById('center-appear-contact').style.display = 'none';
 }
 
 function techClicked(){
     document.getElementById('center-image-holder').style.display == 'none';
+    document.getElementById('center-appear-work').style.display = 'none';
+    document.getElementById('center-appear-about').style.display = 'none';
     document.getElementById('center-appear-tech').style.display = 'flex';
+    document.getElementById('center-appear-careers').style.display = 'none';
+    document.getElementById('center-appear-contact').style.display = 'none';
 }
 
 function careersClicked(){
     document.getElementById('center-image-holder').style.display == 'none';
+    document.getElementById('center-appear-work').style.display = 'none';
+    document.getElementById('center-appear-about').style.display = 'none';
+    document.getElementById('center-appear-tech').style.display = 'none';
     document.getElementById('center-appear-careers').style.display = 'flex';
+    document.getElementById('center-appear-contact').style.display = 'none';
 }
 
 function contactClicked(){
     document.getElementById('center-image-holder').style.display == 'none';
+    document.getElementById('center-appear-work').style.display = 'none';
+    document.getElementById('center-appear-about').style.display = 'none';
+    document.getElementById('center-appear-tech').style.display = 'none';
+    document.getElementById('center-appear-careers').style.display = 'none';
     document.getElementById('center-appear-contact').style.display = 'flex';
 }
